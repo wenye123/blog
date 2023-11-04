7424b81d63706cc396adf74f96515b28f82cc8ed
wenye <1583247975@qq.com>
2023-11-04 22:44:58 +0800

    更新

--- push.sh
+++
@@ -1,6 +0,0 @@
-npm run push
-
-#  使用read命令达到类似bat中的pause命令效果
-echo 按任意键继续
-read -n 1
-echo 继续运行
---
+++ script/compile.ts
@@ -0,0 +1,19 @@
+// import fs from "fs";
+// import mdast from "markdown-ast";
+
+// const path = "./src/2023/003测试.md";
+
+// const str = fs.readFileSync(path).toString();
+
+// const str2 = str.replaceAll(
+//   /!\[(.*?)\]\((.*?)\)/g,
+//   (substring: string, ...args: any[]) => {
+//     // console.log(substring, args)
+
+//     return "111";
+//   }
+// );
+
+// console.log(str2);
+
+// console.log(mdast(str));
